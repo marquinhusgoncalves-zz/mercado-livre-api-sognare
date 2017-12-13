f9bb7cf4e7bb8decd6529ecf038c8d7c52344180
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-12-13 16:42:33 -0200

    Create Client

---
+++ src/client/client.js
@@ -0,0 +1,6 @@
+// Client side application
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'))
