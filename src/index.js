48528ef03ecb7247386fa78485f5fa735e61bab8
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-12-13 18:40:04 -0200

    Add route in server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import renderer from './helpers/renderer'
+import renderer from '../helpers/renderer'
 
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send(renderer())
+app.get('*', (req, res) => {
+  res.send(renderer(req))
 })
 
 app.listen(3000, () => {
